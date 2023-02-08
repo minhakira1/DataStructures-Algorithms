1dea8fc1e7591a6f7448b6be701610980dab6fb4
Minh Conal <vminhdds@gmail.com>
2023-02-08 07:54:24 +0000

    [LeetCode Sync] Runtime - 1 ms (99.32%), Memory - 43.6 MB (100.00%)

---
+++ leetcode-solved/0412-fizz-buzz/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answers = new ArrayList<>();
+        for(int i = 1; i<= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                answers.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                answers.add("Fizz");
+            } else if (i%5 == 0) {
+                answers.add("Buzz");
+            } else {
+                answers.add(i + "");
+            }
+        }
+        return answers;
+        
+    }
+}
