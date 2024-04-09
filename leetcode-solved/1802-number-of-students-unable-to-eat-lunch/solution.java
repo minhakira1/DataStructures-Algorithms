52f079ce08a0a9efebd2513a41f822498767e8cc
Minh Conal <vminhdds@gmail.com>
2024-04-09 03:36:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41 MB (83.53%)

---
+++ leetcode-solved/1802-number-of-students-unable-to-eat-lunch/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int countStudents(int[] students, int[] sandwiches) {
+        int count1Students = 0, count0Students = 0;
+        for (int student : students) {
+            if (student == 0) {
+                count0Students++;
+            } else {
+                count1Students++;
+            }
+        }
+        for (int sandwich : sandwiches) {
+            if ((sandwich == 0 && count0Students == 0)
+                    || (sandwich == 1 && count1Students == 0))
+                break;
+            if (sandwich == 0)
+                count0Students--;
+            else
+                count1Students--;
+        }
+        return count0Students + count1Students;
+    }
+}
