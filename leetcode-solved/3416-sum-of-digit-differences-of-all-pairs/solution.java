48f67610e8bd9d9e52ec301f21e181dbbe10d8a4
Minh Conal <vminhdds@gmail.com>
2024-05-19 02:53:24 +0000

    [LeetCode Sync] Runtime - 28 ms (100.00%), Memory - 57.3 MB (100.00%)

---
+++ leetcode-solved/3416-sum-of-digit-differences-of-all-pairs/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+  public long sumDigitDifferences(int[] nums) {
+    int n = nums.length;
+    int numDigits = String.valueOf(nums[0]).length();
+    int[][] digitCounts = new int[numDigits][10];
+
+    for (int num : nums) {
+      String s = String.valueOf(num);
+      for (int i = 0; i < numDigits; i++) {
+        digitCounts[i][s.charAt(i) - '0']++;
+      }
+    }
+
+    long totalDifferences = 0;
+    for (int i = 0; i < numDigits; i++) {
+      for (int d1 = 0; d1 <= 9; d1++) {
+        for (int d2 = 0; d2 <= 9; d2++) {
+          if (d1 != d2) {
+            totalDifferences += (long) digitCounts[i][d1] * digitCounts[i][d2];
+          }
+        }
+      }
+    }
+    return totalDifferences / 2;
+  }
+}
